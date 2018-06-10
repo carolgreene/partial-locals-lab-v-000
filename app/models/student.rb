f03e1f42be77869ecd73cb3aff3be247ca377ddb
Carol Greene <carolgreene3@gmail.com>
2018-06-10 19:22:55 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,9 +14,9 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def self.search(search)                           #found on Stack overflow. Not sure how this works
-    where "name LIKE ?", "%#{search}%"
-  end
+  #def self.search(search)                           #found on Stack overflow. Not sure how this works
+    #where "name LIKE ?", "%#{search}%"
+  #end
 
    #learn solution
    def self.search(search)
